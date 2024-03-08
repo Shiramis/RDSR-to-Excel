e268d8536168df37e5911788d9b0669a3b7d95b6
Shiramis <133646414+Shiramis@users.noreply.github.com>
2024-03-08 08:53:26 +0200

    Update Extract_Phillips.py

--- Extract_Phillips.py
+++ Extract_Phillips.py
@@ -6,18 +6,6 @@ class PdfFilep:
     def __init__(self, filename, file_path):
         self.filename = filename
         self.file_path = file_path
-
-def check_and_rename_sheet(writer, sheet_name):
-    sheet_name_base = sheet_name
-    sheet_name_counter = 1
-
-    while sheet_name in writer.sheets:
-        sheet_name_counter += 1
-        sheet_name = f"{sheet_name_base} ({sheet_name_counter})"
-
-    return sheet_name
-
-# Set the path to the folder containing Word files
 f = input ('Write the path of the folder with DRL DATA: ')
 folder_path = f
 
@@ -27,8 +15,6 @@ pdf_files = []
 for filename in os.listdir(folder_path):
     if filename.endswith(".pdf"):
         file_path = os.path.join(folder_path, filename)
-
-        # Create a WordFile object and append it to the list
         pdf_file = PdfFilep(filename, file_path)
         pdf_files.append(pdf_file)
 
@@ -51,13 +37,8 @@ with pd.ExcelWriter(output_directory, engine='openpyxl') as writer:
     dfpat = pd.concat(patients, axis=0)
     dfpat.to_excel(writer, sheet_name="Accumulated X-Ray Dose Data")
 
-
-
-# Replace with your desired output directory
-
 def check_and_rename_sheet(writer, sheet_name):
     while sheet_name in writer.sheets:
-        # Extract the base sheet name and the existing counter (if any)
         base_sheet_name, counter = sheet_name, 1
         while True:
             new_sheet_name = f"{base_sheet_name} ({counter})"
